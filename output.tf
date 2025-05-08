3cee5fa477c4de9ee6159d087c2909a39f2db18d
Gurleen Kaur <riarguri25@gmail.com>
2025-05-08 12:23:05 +0530

    Fix Docker login issue in workflow

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "public_ip_address" {
   description = "Public IP address of the Virtual Machine"
-  value       = azurerm_public_ip.pip-veer.ip_address 
+  value       = azurerm_public_ip.example.ip_address 
 }
 
