ec023d2834d901085584a68d28db8fd9d4ec85f3
Gurleen Kaur <riarguri25@gmail.com>
2025-05-12 13:56:22 +0530

    Update: Pushing latest changes

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_public_ip" "example" {
   name                = "pip-veer"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "example" {
